a81d1fc58a59f5ff30a6566ac1a8ec64f63ec396
Kristoffer Klintberg <krstffr@gmail.com>
2014-11-10 16:09:33 +0100

    Version bump v.2.0.2

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   summary: "Group the content in your lists.",
   name: "krstffr:list-grouper",
-  version: "2.0.1",
+  version: "2.0.2",
   git: "https://github.com/krstffr/meteor-list-grouper.git",
 });
 
